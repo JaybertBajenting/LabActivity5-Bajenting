fbf8400a82aa6cfa0852bd2c89266654d6492368
Jaybert <jaybertbajenting99@gmail.com>
2022-12-17 12:46:35 +0800

    Rename LeapYear Components Add LeapYear main function Add exception handling to LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,5 +1,8 @@
 package LeapYear;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
 import javax.swing.*;
 
 public class LeapYearGUI extends JFrame{
@@ -7,6 +10,34 @@ public class LeapYearGUI extends JFrame{
     private JTextField tfYear;
     private JButton btnCheckYear;
 
+
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    int i = Integer.parseInt(tfYear.getText());
+
+                    if(i % 4 == 0){
+
+                        if(i % 100 == 0){
+                            if(i % 400 == 0){
+                                JOptionPane.showMessageDialog(btnCheckYear,"Leap Year");
+                            }
+                        }else{
+                            JOptionPane.showMessageDialog(btnCheckYear,"Leap Year");
+                        }
+
+                    }else{
+                        JOptionPane.showMessageDialog(btnCheckYear,"Not Leap Year");
+                    }
+
+                }catch(Exception f){
+                    JOptionPane.showMessageDialog(btnCheckYear,"Invalid Input");
+                }
+            }
+        });
+    }
 
     public static void main(String[] args) {
 
