bb82dda12054874ed1e6af29dba75be79465a4b2
Jaybert <jaybertbajenting99@gmail.com>
2022-12-17 17:09:14 +0800

    Rename SimpleCalc Components Add SimpleCalc main function Add exception handling to SimpleCalc Complete SimpleCalc Rename FoodOrder Components Add FoodOrder main function Add exception handling to FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -20,6 +20,38 @@ public class FoodOrderGUI extends  JFrame{
     private JRadioButton rb10;
     private JRadioButton rb15;
 
+    FoodOrderGUI(){
+
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+
+                    double total = 0;
+
+                    if(cPizza.isSelected()) total += 100;
+                    if(cBurger.isSelected()) total += 80;
+                    if(cFries.isSelected()) total += 65;
+                    if(cSoftDrinks.isSelected()) total += 55;
+                    if(cTea.isSelected())total += 50;
+                    if(cSundae.isSelected()) total += 40;
+
+
+
+                    if(rb5.isSelected()) total = total - total * 0.05;
+                    if(rb10.isSelected()) total = total - total * 0.10;
+                    if(rb15.isSelected()) total = total - total * 0.15;
+
+                    DecimalFormat df = new DecimalFormat(".00");
+
+                    JOptionPane.showMessageDialog(btnOrder,"The total price is " + df.format(total));
+
+                }catch (Exception f){
+                    JOptionPane.showMessageDialog(btnOrder,"Invalid Input");
+                }
+            }
+        });
+    }
     public static void main(String[] args) {
         FoodOrderGUI f = new FoodOrderGUI();
         f.setTitle("Food Ordering System");
