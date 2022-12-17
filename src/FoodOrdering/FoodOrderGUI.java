85d578586ccc1391e23161104892013557a44a0a
Jaybert <jaybertbajenting99@gmail.com>
2022-12-17 17:07:49 +0800

    Rename SimpleCalc Components Add SimpleCalc main function Add exception handling to SimpleCalc Complete SimpleCalc Rename FoodOrder Components

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,10 +1,28 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.text.DecimalFormat;
 
-public class FoodOrderGUI {
-    private JPanel panel1;
-    private JCheckBox pizzaCheckBox;
-    private JRadioButton noneRadioButton;
-    private JButton orderButton;
+public class FoodOrderGUI extends  JFrame{
+    private JPanel panelMain;
+    private JCheckBox cPizza;
+    private JRadioButton rbNone;
+    private JButton btnOrder;
+    private JCheckBox cBurger;
+    private JCheckBox cFries;
+    private JCheckBox cSoftDrinks;
+    private JCheckBox cTea;
+    private JCheckBox cSundae;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
+
+
+
 }
+
+
+
